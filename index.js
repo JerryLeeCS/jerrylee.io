c221226735a118c9027ab4d5a8611797afc69f85
Jerry Lee <jerryleeatwork@gmail.com>
2019-09-04 13:59:56 -0700

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+var heart = document.getElementById('heart-x');
+var container = document.getElementById('card-container');
+var front = document.getElementById('front');
+heart.addEventListener('click', function() {
+});
