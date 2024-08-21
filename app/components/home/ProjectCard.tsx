ba5ea77e80b92e4f5f3ec22f45567495c524e4ca
Astral <JerryDeKo@gmail.com>
2024-08-21 16:30:48 -0700

    Moved dates to the right of the ProjectCard.

--- app/components/home/ProjectCard.tsx
+++ app/components/home/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ project }: Props) => {
       <div className="group border border-neutral-100 hover:bg-neutral-100 hover:text-black border-solid p-4">
         <h1 className="text-lg font-bold mb-2">{project.title}</h1>
         <h2 className="text-sm text-neutral-400 mb-2">{project.description}</h2>
-        <div className="text-xs text-neutral-400">
+        <div className="text-xs text-neutral-400 text-right">
           <span>
             {getMonthYear(project.startDate)} -{" "}
             {project.endDate ? getMonthYear(project.endDate) : "Present"}
